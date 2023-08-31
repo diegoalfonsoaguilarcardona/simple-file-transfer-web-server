83d6ce65978005d46dbb4e726c0d7bb501a41db2
Diego Aguilar <diego.alfonso.aguilar.cardona@gmail.com>
2023-08-30 22:03:45 -0500

    now I can transfer files from my pc to the client

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const multer  = require('multer');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
-  },
-  filename: function (req, file, cb) {
-    const ext = path.extname(file.originalname);
-    const basename = path.basename(file.originalname, ext);
-    const timestamp = Date.now();
-    cb(null, `${basename}-${timestamp}${ext}`);
-  }
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function(req, file, cb) {
+        const ext = path.extname(file.originalname);
+        const basename = path.basename(file.originalname, ext);
+        const timestamp = Date.now();
+        cb(null, `${basename}-${timestamp}${ext}`);
+    }
 })
 
 const upload = multer({ storage: storage });
@@ -22,10 +23,21 @@ const upload = multer({ storage: storage });
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/upload', upload.single('file'), (req, res) => {
-    // You can access the uploaded file via req.file
     console.log(req.file);
 
     res.send('File uploaded successfully.');
+});
+
+app.get('/listfiles', (req, res) => {
+    fs.readdir(path.join(__dirname, 'public'), (err, files) => {
+        if(err) {
+            console.log(err);
+            res.status(500).send('An error occurred while retrieving files');
+        } else {
+            let fileList = files.map(file => path.join('/',file));
+            res.send(fileList);
+        }
+    });
 });
 
 app.listen(3000, () => {
